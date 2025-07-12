2aff012bdfb568eeedfa1221786600e07c7c0072
santoshk66 <santosh.k66@gmail.com>
2025-07-12 17:22:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs').promises;
-const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
+const { PDFDocument, rgb, StandardFonts, degrees } = require('pdf-lib');
 const pdfParse = require('pdf-parse');
 const { v4: uuidv4 } = require('uuid');
 
